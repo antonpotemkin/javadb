2efecb8c69c909a0b589e411112efe0ed829c38b
Anton Potemkin <79133891911@yandex.ru>
2022-12-03 00:29:49 +0700

    add offset reader from file

--- src/main/java/ru/javadb/Main.java
+++ src/main/java/ru/javadb/Main.java
@@ -1,13 +1,19 @@
 package ru.javadb;
 
-import ru.javadb.storage.Storage;
+import ru.javadb.utils.FileUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        Storage storage = new Storage(".");
-        storage.put("key1", "value1");
-        storage.put("key2", "value2");
-        storage.put("key1", "value3");
-        System.out.println(storage.get("key1"));
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = "ДомРаботаУлица".getBytes(StandardCharsets.UTF_8);
+        Path filePath = Paths.get("test.txt");
+        Files.write(filePath, bytes);
+        String read = FileUtils.read(filePath, 3, 6);
+        System.out.println(read);
     }
 }
--- src/main/java/ru/javadb/utils/FileUtils.java
+++ src/main/java/ru/javadb/utils/FileUtils.java
@@ -1,6 +1,8 @@
 package ru.javadb.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -15,6 +17,28 @@ public final class FileUtils {
             Files.delete(root);
         } catch (IOException e) {
             System.out.println("error while delete file " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String read(Path path, long offset, int size) {
+        try {
+            BufferedReader bufferedReader = Files.newBufferedReader(path);
+            long skip = bufferedReader.skip(offset);
+            if (offset != skip) {
+                System.out.println("error while skip offset");
+                throw new RuntimeException("error while skip offset");
+            }
+            CharBuffer buffer = CharBuffer.allocate(size);
+            int read = bufferedReader.read(buffer);
+            if (read != size) {
+                System.out.println("error while read offset");
+                throw new RuntimeException("error while read offset");
+            }
+            buffer.flip();
+            return buffer.toString();
+        } catch (IOException e) {
+            System.out.println("error while read file " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
